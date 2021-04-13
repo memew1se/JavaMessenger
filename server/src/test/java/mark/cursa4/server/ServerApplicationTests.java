0aa57dcbd8d9b87e81c380a0be21e9c352c30d89
Mark <m4rk.l1kh4ch3v@gmail.com>
2021-04-13 22:19:56 +0300

    Initial commit

---
+++ server/src/test/java/mark/cursa4/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mark.cursa4.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
