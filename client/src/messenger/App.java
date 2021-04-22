a65d8a5d789c1981a1483c068ffec75b33eaaf95
Mark <gogomecourier@gmail.com>
2021-04-22 15:38:33 +0300

    Первоначальная настройка клиента

--- client/src/messenger/App.java
+++ client/src/messenger/App.java
@@ -1,19 +1,22 @@
-package sample;
+package messenger;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
+        primaryStage.setTitle("JavaMessenger");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
     }
 
 
---
+++ client/src/messenger/controllers/Controller.java
@@ -0,0 +1,4 @@
+package messenger.controllers;
+
+public class Controller {
+}
